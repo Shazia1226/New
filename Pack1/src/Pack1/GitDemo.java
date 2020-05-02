d559789a9c08f772d863b812acce8664c490b4b1
Shazia1226 <63747134+Shazia1226@users.noreply.github.com>
2020-05-02 16:44:34 -0700

    Revert "Added third line in GitDemo"
    This reverts commit bacf5f963c419666cf6af0b56412593fd30e1241.

--- Pack1/src/Pack1/GitDemo.java
+++ Pack1/src/Pack1/GitDemo.java
@@ -5,8 +5,6 @@ public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("my second experience");
 		System.out.println("my git experience");
-	    System.out.println("added third line");
 	}
-	
 
 }
