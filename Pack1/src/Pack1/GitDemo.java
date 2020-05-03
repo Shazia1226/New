b2e944c1214da14096a0a7539e6b3abf971a419e
Shazia1226 <63747134+Shazia1226@users.noreply.github.com>
2020-05-02 18:47:22 -0700

    added x,y,z in git class

--- Pack1/src/Pack1/GitDemo.java
+++ Pack1/src/Pack1/GitDemo.java
@@ -1,7 +1,7 @@
 package Pack1;
 
 public class GitDemo {
-
+int x=100,y=200,z;
 	public static void main(String[] args) {
 		System.out.println("my second experience");
 		System.out.println("my git experience");
